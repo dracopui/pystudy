087fc48c2d3d7bdcac1d77c04fd58409ab4edd4a
longbui <94639156+dracopui@users.noreply.github.com>
2023-03-10 11:58:43 +0900

    Add files via upload

---
+++ buinhatlong_0310_05.py
@@ -0,0 +1,19 @@
+#컴퓨터공학부 2021243170 부이낫롱
+# 작성일: 2023-03-10
+# 파일명:buinhatlong_0310_05.py
+# 제목: 변수의 션언
+
+boolVar = True
+aaa= True
+intVar = 1
+b=3
+floatVar = 3.14
+strVar = "SUNMOON"
+var1=100
+var1=var1+200
+
+print(type(boolVar))
+print(type(aaa))
+print(type(intVar))
+print(type(b))
+print(var1)
